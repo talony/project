29b9d09298a15ca9f26c2bb1c538b1afd85f981f
talony <wecad3050@gmail.com>
2016-03-28 23:00:40 +0200

    main

---
+++ project/src/project/work.java
@@ -0,0 +1,31 @@
+package project;
+import javafx.event.ActionEvent;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
+public class work extends Application{
+
+    public static void main(String[] args) {
+    	launch(args);
+
+        
+    }
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		
+	}
+}
